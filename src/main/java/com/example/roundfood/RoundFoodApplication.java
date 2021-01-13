d4997f34bfeb1df18b70e75f42540ec5e246d814
krisztianAlt <alt.krisztian@gmail.com>
2021-01-13 15:25:42 +0100

    Create Spring Boot project

---
+++ src/main/java/com/example/roundfood/RoundFoodApplication.java
@@ -0,0 +1,13 @@
+package com.example.roundfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoundFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoundFoodApplication.class, args);
+	}
+
+}
