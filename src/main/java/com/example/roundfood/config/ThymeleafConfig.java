4dae65b5b5c198b53ed8a10eae1796d11f78bf2d
krisztianAlt <alt.krisztian@gmail.com>
2021-01-15 03:44:56 +0100

    UI: add navbar, carousel, footer

---
+++ src/main/java/com/example/roundfood/config/ThymeleafConfig.java
@@ -0,0 +1,16 @@
+package com.example.roundfood.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
+
+}
